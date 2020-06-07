642079f69f0c1aa453876d2129fce9080140c6bf
songjiajun <1772696091@qq.com>
2020-06-07 18:02:46 +0800

    架构搭建

--- app/src/main/java/com/hbcx/bjckyh/ui/LogonActivity.kt
+++ app/src/main/java/com/hbcx/bjckyh/ui/LogonActivity.kt
@@ -3,10 +3,12 @@ package com.hbcx.bjckyh.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hbcx.bjckyh.R
+import org.jetbrains.anko.toast
 
-class MainActivity : BaseActivity() {
+class LogonActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_logon)
     }
 }
