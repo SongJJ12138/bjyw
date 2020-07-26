b2a7dc59d52c962899129b7286c8c0d1ad0d2ba7
songjiajun <xEC*mX3g9WTp3HG>
2020-07-26 09:48:52 +0800

    。。

--- app/src/main/java/com/bjyw/bjckyh/network/Api.kt
+++ app/src/main/java/com/bjyw/bjckyh/network/Api.kt
@@ -1,7 +1,7 @@
 package com.bjyw.bjckyh.network
 
 object Api {
-    const val BASE_URL = "http://101.200.167.139:8086/bjyw-controller/" //外网地址
+    const val BASE_URL = "http://101.200.167.139:8085/bjyw-controller/" //外网地址
     const val LOGIN = "auth/token" //登陆
     const val CHECK_CARD = "card/isCard" //检查打卡
     const val GET_ORDER = "ticket/getOrder" //获取工单
